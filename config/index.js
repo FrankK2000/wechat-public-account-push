0b81e32e2312023e1e1f390799da787212597e6a
FrankK2000 <114325785+FrankK2000@users.noreply.github.com>
2022-10-20 20:43:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wx17e1b462902d7e3c,oaerH5lE3Mo5tSUmEgx15IjxBOKo',
+  APP_ID: 'wx17e1b462902d7e3c',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
@@ -65,7 +65,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '亲爱的小莉',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oaerH5kqDs0j6oY8O9BqkaKrlzPc',
+      id: 'oaerH5kqDs0j6oY8O9BqkaKrlzPc','oaerH5lE3Mo5tSUmEgx15IjxBOKo',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'w5_-qGqt_A5XsMoIj3yfc6Rpd4MIF6KIAc-e54UFsg0',
       // 所在省份
